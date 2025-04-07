6b066399bf1856b3d0cc4819071b86c82765c76f
ironbilgi <ironbilgi@icloud.com>
2025-04-07 20:41:44 +0000

    Refactor addTransaction function to properly initialize recent transactions and account objects

--- mainFunc/logs.py
+++ mainFunc/logs.py
@@ -2,4 +2,11 @@ from mainFunc.userInput import mainInterface
 from mainFunc.accountFunc import Account
 
 def addTransaction():
-    recentTransactions = mainInterface().append
+    recentTransactions = mainInterface()
+    # Creates an account object
+    account = Account() 
+    # Creates a transaction object  
+    transaction = account.transactionLog
+    # Creates a transaction time object
+    transactionTime = account.transactionLogTime
+    # Creates a transaction amount object
